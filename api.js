49905a0131e9c083f138a551032c98a3370670d4
Ben Tranter <bwtrante@lakeheadu.ca>
2015-02-02 01:51:58 -0500

    Update DB name to reflect example

--- api.js
+++ api.js
@@ -3,7 +3,7 @@
 var thinky = require('thinky')({
     host: process.env.RDB_HOST || 'localhost',
     port: parseInt(process.env.RDB_PORT || 28015),
-    db:   process.env.RDB_DB || 'StudyFast'
+    db:   process.env.RDB_DB || 'People'
 });
 var r = thinky.r;
 
